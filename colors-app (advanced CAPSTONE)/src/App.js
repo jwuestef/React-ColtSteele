31d8329e627bafb026ab4d046a474b5108de043d
jwuestef <jwuestef@gmail.com>
2019-10-23 20:19:07 -0400

    Pause point

---
+++ colors-app (advanced CAPSTONE)/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './App.css';
+import seedColors from './seedColors';
+import Palette from './Palette';
+import { generatePalette } from './colorHelpers';
+
+
+
+function App() {
+    return (
+        <div className="App">
+            <Palette palette={generatePalette(seedColors[4])} />
+        </div>
+    );
+}
+
+
+
+export default App;
