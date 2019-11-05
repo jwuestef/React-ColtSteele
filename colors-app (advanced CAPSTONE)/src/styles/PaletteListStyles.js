5370074b04dd6a87dfd1b092253d93384fed0b5b
Jeremy Wuestefeld <jwuestef@gmail.com>
2019-11-05 09:52:33 -0500

    added SVG background

--- colors-app (advanced CAPSTONE)/src/styles/PaletteListStyles.js
+++ colors-app (advanced CAPSTONE)/src/styles/PaletteListStyles.js
@@ -1,12 +1,15 @@
 import sizes from './sizes'
+import bg from './bg.svg'
 
 export default {
     root: {
-        backgroundColor: "blue",
         height: "100vh",
         display: "flex",
         alignItems: "flex-start",
-        justifyContent: "center"
+        justifyContent: "center",
+        backgroundColor: "#394bad",
+        backgroundImage: `url(${bg})`,
+        overflow: "scroll"
     },
     container: {
         width: "50%",
@@ -42,7 +45,7 @@ export default {
         },
         [sizes.down('xs')]: {
             gridTemplateColumns: "repeat(1, 100%)",
-            gridGap: "1rem",
+            gridGap: "1.4rem",
         },
     }
 }
